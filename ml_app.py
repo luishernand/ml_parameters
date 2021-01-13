39a846692dc80c37ffd92219e10f2d0f137070c5
Luis Hernández <53943021+luishernand@users.noreply.github.com>
2021-01-13 06:53:37 -0600

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -9,7 +9,7 @@ Created on Mon Jan 11 14:56:11 2021
 import streamlit as st
 import pandas as pd
 import numpy as np
-#from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error , r2_score
 from sklearn.datasets import load_boston
